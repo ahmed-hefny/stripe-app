220b814212327294947a25658539831dd21779b8
ahmed-hefny <ahmedhgad@gmail.com>
2023-12-14 14:17:18 +0200

    init commit

---
+++ src/stripe.js
@@ -0,0 +1,198 @@
+const MAX_CUSTOMER_LIST = 100
+const DAY_IN_MS = 24 * 60 * 60 * 1000
+
+// eslint-disable-next-line no-unused-vars
+class Stripe {
+  constructor (stripeKey) {
+    this.stripeKey = stripeKey
+    this.baseUrl = 'https://api.stripe.com/v1'
+    this.inProgress = false
+  }
+
+  async getReq (path, params = {}) {
+    const url = `${this.baseUrl}/${path}`
+    const queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&')
+    const endpoint = queryString
+      ? `${url}?${queryString}`
+      : url
+    const response = await fetch(endpoint, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${this.stripeKey}`,
+      },
+    })
+
+    return response.json()
+  }
+
+  async postReq (path, body = {}) {
+    const url = `${this.baseUrl}/${path}`
+    const urlSearch = new URLSearchParams(body)
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: `Bearer ${this.stripeKey}`,
+      },
+      body: urlSearch.toString(),
+    }
+
+    const response = await fetch(url, requestOptions)
+
+    return response.json()
+  }
+
+  async customersList (params = { limit: MAX_CUSTOMER_LIST }) {
+    return this.getReq('customers', params)
+  }
+
+  async getAllCustomer () {
+    let startAfter = null
+    let hasMore
+    const allCustomers = []
+
+    do {
+      const customers = startAfter
+        ? await this.customersList({ limit: MAX_CUSTOMER_LIST, starting_after: startAfter })
+        : await this.customersList({ limit: MAX_CUSTOMER_LIST })
+
+      allCustomers.push(...customers.data)
+      startAfter = customers.data[customers.data.length - 1].id
+      hasMore = customers.has_more
+    } while (hasMore)
+
+    return allCustomers
+  }
+
+  async getExtendedCustomers () {
+    const customers = await this.getAllCustomer()
+
+    const promises = Promise.map(customers, async customer => {
+      const [ { data: paymentMethod }, { data: charges } ] = await Promise.all([
+        this.getCustomerPaymentMethod(customer),
+        this.getCustomerCharges(customer)
+      ])
+
+      const extendedCustomer = {
+        ...customer,
+        paymentMethod: paymentMethod[0],
+        charged: false,
+        statuses: [],
+        charges
+      }
+
+      this.setCustomerStatuses(extendedCustomer)
+
+      return extendedCustomer
+    }, { concurrency: 40 })
+
+    return await Promise.all(promises)
+  }
+
+  setCustomerStatuses (customer) {
+    const statuses = []
+    const { paymentMethod, charges } = customer
+
+    // Card statuses
+    if (paymentMethod) {
+      const { card } = paymentMethod
+      const cardDate = new Date(card.exp_year, card.exp_month - 1)
+      const currentDate = new Date()
+
+      if (cardDate.getTime() < currentDate.getTime()) {
+        statuses.push(this.createStatus('Expired Card', 'warning'))
+        customer.cardExpired = true
+      } else {
+        statuses.push(this.createStatus('Valid Card', 'success'))
+        customer.hasValidCard = true
+      }
+    } else {
+      statuses.push(this.createStatus('No card', 'danger'))
+    }
+
+    // Charge statuses
+    if (charges.length) {
+      const hasRefundedCharge = charges.find(item => item.refunded)
+
+      if (hasRefundedCharge) {
+        statuses.push(this.createStatus('Refunded', 'secondary'))
+        customer.isRefunded = true
+      }
+      const hasDisputedCharge = charges.find(item => item.disputed)
+
+      if (hasDisputedCharge) {
+        statuses.push(this.createStatus('Disputed', 'danger'))
+        customer.isDisputed = true
+      }
+      const chargedToday = charges.filter(item => item.status === 'succeeded')
+        .find(item => {
+          const yesterdayMs = Date.now() - DAY_IN_MS
+
+          return item.created * 1000 > yesterdayMs
+        })
+
+      if (chargedToday) {
+        statuses.push(this.createStatus('Charged Today', 'info'))
+        customer.chargedToday = true
+      }
+    }
+
+    customer.statuses = statuses
+  }
+
+  async getCustomerPaymentMethod (customer) {
+    return await this.getReq(`customers/${customer.id}/payment_methods`)
+  }
+
+  async getCustomerCharges (customer) {
+    return await this.getReq('charges', { limit: 100, customer: customer.id })
+  }
+
+  async chargeCustomers (customers, body, concurrency = 1) {
+    const promises = Promise.map(customers, async customer => {
+      if (this.inProgress) {
+        try {
+          if (customer.paymentMethod) {
+            const payload = {
+              amount: body.amount,
+              currency: body.currency,
+              customer: customer.id,
+              description: 'Subscription',
+              source: customer.paymentMethod.id
+            }
+            const createdCharge = await this.postReq('charges', payload)
+
+            customer.charged = createdCharge.status === 'succeeded'
+            customer.chargeError = createdCharge?.error?.decline_code || createdCharge?.outcome?.reason
+            customer.charge = createdCharge
+          } else {
+            customer.chargeError = 'Customer has no payment method'
+          }
+        } catch (e) {
+          customer.chargeError = 'INTERNAL ERROR - ' + e.message
+        }
+
+        return customer
+      }
+    }, { concurrency })
+
+    return await promises.all(promises)
+  }
+
+  async checkKey () {
+    try {
+      const response = await this.getReq('customers', { limit: 1 })
+
+      if (response.error) {
+        throw response.error
+      }
+    } catch (e) {
+      return e.message
+    }
+  }
+
+  createStatus (name, color) {
+    return { name, color }
+  }
+}
---
+++ src/ui.js
@@ -0,0 +1,278 @@
+/* global swal Stripe  */
+const $ = selector => document.querySelector(selector)
+let ALL_CUSTOMERS
+let STRIPE
+
+const els = {
+  $applyFilter: $('#applyFilter'),
+  $tableData: $('#table-records'),
+  $amount: $('#chargeAmount'),
+  $currency: $('#currency'),
+  $concurrency: $('#concurrency'),
+  $expectedTotal: $('#expectedTotalAmount'),
+  $actualTotal: $('#actualTotalAmount'),
+  $chargeBtn: $('#charge')
+}
+const actionButtons = {
+  charge: 'Charge',
+  stop: 'Stop',
+}
+
+const displayCustomers = async () => {
+  const key = $('#secretKey').value
+  STRIPE = new Stripe(key)
+
+  if (!key.length) {
+    swal('Oops', 'Key is Empty', 'error')
+
+    return
+  }
+
+  const keyError = await STRIPE.checkKey()
+
+  if (keyError) {
+    swal('Oops', keyError, 'error')
+
+    return
+  }
+
+  startLoader(els.$applyFilter)
+  els.$chargeBtn.disabled = true
+  els.$chargeBtn.innerHTML = getSpinnerHTML('primary', 1)
+
+  els.$tableData.innerHTML = `<th class="text-center" colspan="5">${getSpinnerHTML('secondary', 15)}</th>`
+  setSecretKey(key)
+  const data = await STRIPE.getExtendedCustomers()
+  els.$tableData.innerHTML = ''
+
+  ALL_CUSTOMERS = filterCustomers(data)
+  ALL_CUSTOMERS.forEach(renderObj)
+  calculateTotal()
+
+  els.$chargeBtn.disabled = false
+  els.$chargeBtn.innerHTML = actionButtons.charge
+
+  stopLoad(els.$applyFilter)
+}
+
+const filterCustomers = (customers) => {
+  const excludedIds = $('#excludedIDS').value.trim().split(/(,|\s+|\n)/).map(item => item.trim()).filter(Boolean)
+  const refundedBox = $('#refundedBox').checked
+  const disputedBox = $('#disputedBox').checked
+  const expiredBox = $('#expiredBox').checked
+  const chargedBox = $('#chargedBox').checked
+  const validBox = $('#validBox').checked
+
+  return customers.filter(cus =>
+    (!excludedIds.includes(cus.id)) &&
+      (refundedBox ? !cus.isRefunded : true) &&
+      (disputedBox ? !cus.isDisputed : true) &&
+      (expiredBox ? !cus.cardExpired : true) &&
+      (validBox ? cus.hasValidCard : true) &&
+      (chargedBox ? !cus.chargedToday : true)
+  )
+}
+
+const downloadChargedCustomers = () => {
+  const data = ALL_CUSTOMERS.map((cus, i) => ({
+    '#': i + 1,
+    id: cus.id,
+    email: cus.email,
+    charged: cus.charged ? 'Yes' : 'No',
+    error: cus.chargeError
+  }))
+
+  const convertToCSV = results => {
+    const header = Object.keys(results[0]).join(',')
+    const rows = results.map(obj => Object.values(obj).join(','))
+
+    return `${header}\n${rows.join('\n')}`
+  }
+
+  const downloadCSV = results => {
+    const csvString = convertToCSV(results)
+    const blob = new Blob([csvString], { type: 'text/csv' })
+    const url = window.URL.createObjectURL(blob)
+
+    const a = document.createElement('a')
+    a.href = url
+    a.download = 'data.csv'
+    document.body.appendChild(a)
+    a.click()
+
+    window.URL.revokeObjectURL(url)
+  }
+
+  if (data.length) {
+    downloadCSV(data)
+  } else {
+    downloadEmptyFile()
+  }
+}
+const downloadEmptyFile = () => {
+  const emptyCSV = ''
+  const emptyBlob = new Blob([emptyCSV], { type: 'text/csv' })
+
+  const emptyUrl = window.URL.createObjectURL(emptyBlob)
+
+  const a = document.createElement('a')
+  a.href = emptyUrl
+  a.download = 'data.csv'
+  document.body.appendChild(a)
+  a.click()
+
+  window.URL.revokeObjectURL(emptyUrl)
+}
+const changeChargeBtnText = (name) => els.$chargeBtn.innerText = name
+
+const chargeBtnCustomers = () => {
+  if (els.$chargeBtn.innerText === actionButtons.charge) {
+    swal(`Are you sure you want to ${els.$chargeBtn.innerText}`, {
+      buttons: {
+        cancel: true,
+        confirm: true,
+      }
+    })
+      .then((value) => {
+        if (value) {
+          chargeCustomers()
+        }
+      })
+  } else {
+    stopChargingCustomers()
+  }
+}
+
+
+const stopChargingCustomers = () => {
+  STRIPE.inProgress = false
+}
+
+const chargeCustomers = async () => {
+  const amount = $('#chargeAmount').value * 100
+
+  if (amount > 0) {
+    changeChargeBtnText(actionButtons.stop)
+    let actualAmount = 0
+    const currency = els.$currency.value
+    const concurrency = +els.$concurrency.value
+    STRIPE.inProgress = true
+    els.$tableData.innerHTML = `<th class="text-center" colspan="5">${getSpinnerHTML('secondary', 15)}</th>`
+    ALL_CUSTOMERS.forEach(cus => {cus.charged = false})
+    const chargedCustomersList = await STRIPE.chargeCustomers(ALL_CUSTOMERS, { amount, currency }, concurrency)
+    chargedCustomersList.filter(ch => ch?.charged).forEach(customer => {
+      actualAmount += customer?.charge?.amount_captured / 100
+    })
+    els.$tableData.innerHTML = ''
+
+    ALL_CUSTOMERS.forEach(renderObj)
+    els.$actualTotal.innerText = `${actualAmount} ${els.$currency.value}`
+    changeChargeBtnText(actionButtons.charge)
+
+    downloadChargedCustomers()
+  } else {
+    swal('Oops', 'Charge amount is invalid', 'error')
+  }
+}
+
+const calculateTotal = () => {
+  const amount = $('#chargeAmount').value
+  const currency = $('#currency').value
+  els.$expectedTotal.innerText = ` ${amount ? amount * ALL_CUSTOMERS?.length : '0'} ${currency}`
+}
+
+const getStatuesHTML = (list = []) => {
+  let html = ''
+  list.forEach(status => {
+    html += `<span class="me-2 mt-2 mt-lg-0 badge d-inline-block bg-${status.color}">${status.name}</span>`
+  })
+
+  return html
+}
+
+const renderObj = (obj, index) => {
+  const tr = document.createElement('tr')
+  const bgColor = obj.charged
+    ? 'success'
+    : obj.chargeError
+      ? 'danger'
+      : 'secondary'
+  const chargedStatus = obj.charged
+    ? 'Yes'
+    : obj.chargeError
+      ? obj.chargeError
+      : 'No'
+
+  const statusHtml = getStatuesHTML(obj.statuses)
+
+  tr.innerHTML = `
+    <th scope="row">${index + 1}</th>
+    <td >${obj.id}</td>
+    <td>${obj.email}</td>
+    <td> 
+      <div class="d-flex flex-wrap">
+        ${statusHtml}
+      </div>
+    </td>
+    <td>
+      <span class="badge bg-${bgColor}">${chargedStatus}</span>
+    </td>
+   `
+  els.$tableData.appendChild(tr)
+}
+
+const getSpinnerHTML = (color, size = '2') => `<div style="width: ${size}rem; height: ${size}rem;" class="spinner-border text-${color}" role="status">
+<span class="visually-hidden">Loading...</span>
+</div>`
+
+const startLoader = el => {
+  el.disabled = true
+  el.querySelector('[role="loader"]').classList.remove('d-none')
+}
+const stopLoad = el => {
+  el.disabled = false
+  el.querySelector('[role="loader"]').classList.add('d-none')
+}
+
+const setSecretKey = (secretKey) => {
+  const date = new Date()
+  date.setTime(date.getTime() + (24 * 60 * 60 * 1000))
+  const expires = 'expires=' + date.toUTCString()
+  document.cookie = 'secretKey' + '=' + secretKey + ';' + expires + ';path=/'
+}
+
+const getSecretKey = () => {
+  const cookieName = 'secretKey='
+  const decodedCookie = decodeURIComponent(document.cookie)
+  const cookieArray = decodedCookie.split(';')
+
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i]
+
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1)
+    }
+
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length)
+    }
+  }
+
+  return null
+}
+
+// init listeners
+const initListeners = () => {
+  els.$amount.addEventListener('keyup', calculateTotal)
+  els.$currency.addEventListener('change', calculateTotal)
+  els.$applyFilter.addEventListener('click', displayCustomers)
+  els.$chargeBtn.addEventListener('click', chargeBtnCustomers)
+}
+
+const init = () => {
+  initListeners()
+
+  $('#secretKey').value = getSecretKey()
+}
+
+init()
