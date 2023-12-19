b751b678602ef62edaa898f2c3febc702c1269fe
Brostafa <mostafahefny2010@gmail.com>
2023-12-19 04:34:36 +0200

    fix: use express.json instead of body parser

--- server/index.js
+++ server/index.js
@@ -13,10 +13,9 @@ const {
 const PORT = process.env.PORT || 3000
 const PUBLIC_PATH = path.join(__dirname, '..', 'public')
 
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 app.use(cors())
 app.use(compression())
-app.use(express.bodyParser({ limit: '50mb' }))
 app.use(express.static(PUBLIC_PATH))
 /**
  * @route POST /api/list-customers
